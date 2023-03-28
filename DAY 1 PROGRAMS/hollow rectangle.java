52b6bca9963c8110986b876e8264a1abd8ef8ec1
KIRAN192124063 <112570391+KIRAN192124063@users.noreply.github.com>
2023-03-28 21:46:48 +0530

    Create hollow rectangle.java

---
+++ DAY 1 PROGRAMS/hollow rectangle.java
@@ -0,0 +1,23 @@
+import java.io.*;
+  
+class GFG {
+      
+    // Function to print hollow rectangle
+    static void print_rectangle(int n, int m)
+    {
+        int i, j;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                if (i == 1 || i == n || 
+                    j == 1 || j == m)            
+                    System.out.print("*");            
+                else
+                    System.out.print(" ");            
+            }
+            System.out.println();
+        }
+       
+    }
+       

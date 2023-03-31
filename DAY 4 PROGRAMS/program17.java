1f8a7cf301be94e9a67f8d0334bee7856dfe5475
KIRAN192124063 <112570391+KIRAN192124063@users.noreply.github.com>
2023-03-31 21:00:44 +0530

    Create program17.java

---
+++ DAY 4 PROGRAMS/program17.java
@@ -0,0 +1,17 @@
+class GFG
+{
+    // Function to find the nth odd number
+    static int nthOdd(int n)
+    {
+        return (2 * n - 1);
+    }
+     
+    // Driver code
+    public static void main(String [] args)
+    {
+        int n = 10;
+        System.out.println(nthOdd(n));
+         
+    }
+}
+ 
